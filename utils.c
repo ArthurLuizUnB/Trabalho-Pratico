da74072c8de74a9bc710fe3dabf62484395c6db2
PauloViniciusP <nexz265@gmail.com>
2026-01-30 16:49:44 -0300

    feat(utils): add CPF validation logic
    Adiciona algoritmo de validacao de digitos verificadores (Modulo 11) e sanitizacao de input.
    
    Co-authored-by: Arthur Luiz <arthurluizsg@gmail.com>
    Co-authored-by: Ryan Lira <rlira046@gmail.com>

--- utils.c
+++ utils.c
@@ -31,3 +31,43 @@ void *alloc_check(size_t tamanho) {
     }
     return ptr;
 }
+
+int validar_cpf(char *cpf) {
+    char numeros[12];
+    int i, j = 0;
+
+    // Sanitizacao: Extrai apenas os numeros do input (ignora pontos/tracos)
+    for (i = 0; cpf[i] != '\0'; i++) {
+        if (isdigit(cpf[i])) {
+            numeros[j++] = cpf[i];
+            if (j > 11) return 0; // CPF maior que o permitido
+        }
+    }
+    numeros[j] = '\0';
+
+    if (strlen(numeros) != 11) return 0;
+
+    // Blacklist de CPFs invalidos comuns (todos digitos iguais)
+    if (strcmp(numeros, "00000000000") == 0 || strcmp(numeros, "11111111111") == 0 ||
+        strcmp(numeros, "22222222222") == 0 || strcmp(numeros, "33333333333") == 0 ||
+        strcmp(numeros, "44444444444") == 0 || strcmp(numeros, "55555555555") == 0 ||
+        strcmp(numeros, "66666666666") == 0 || strcmp(numeros, "77777777777") == 0 ||
+        strcmp(numeros, "88888888888") == 0 || strcmp(numeros, "99999999999") == 0)
+        return 0;
+
+    // Calculo do 1o Digito Verificador
+    int soma = 0;
+    for (i = 0; i < 9; i++) soma += (numeros[i] - '0') * (10 - i);
+    int resto = (soma * 10) % 11;
+    if (resto == 10 || resto == 11) resto = 0;
+    if (resto != (numeros[9] - '0')) return 0;
+
+    // Calculo do 2o Digito Verificador
+    soma = 0;
+    for (i = 0; i < 10; i++) soma += (numeros[i] - '0') * (11 - i);
+    resto = (soma * 10) % 11;
+    if (resto == 10 || resto == 11) resto = 0;
+    if (resto != (numeros[10] - '0')) return 0;
+
+    return 1; // Valido
+}
